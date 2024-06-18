56ad8d51204845a3fb5a3362add524a161912a4b
yassun7010 <yassun7010@outlook.com>
2024-06-19 07:37:15 +0900

    fix: error handling.

--- src/envix/cli/__main__.py
+++ src/envix/cli/__main__.py
@@ -1,9 +1,19 @@
+from logging import getLogger
+
 from envix.cli import App
+from envix.exception import EnvixLoadEnvsError
+
+logger = getLogger(__name__)
 
 
 def main() -> None:
     try:
         App.run()
+
+    except EnvixLoadEnvsError as e:
+        for error in e.errors:
+            logger.error(error)
+        exit(1)
 
     except Exception:
         exit(1)
